13d6a03bb09047b066aafa213e331855206dc2d2
JoharyHugo <joharyny@gmail.com>
2023-06-30 12:22:56 +0200

    ok

--- Test/src/java/etu2021/Framework/model/Login.java
+++ Test/src/java/etu2021/Framework/model/Login.java
@@ -4,10 +4,39 @@
  */
 package etu2021.Framework.model;
 
+import etu2021.Framework.annotation.Url;
+import etu2021.Framework.loadview.ModelView;
+
 /**
  *
  * @author johary
  */
 public class Login {
-    
+    String name;
+    String mdp;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+    @Url(lien="connect")
+    public ModelView Connected(){
+        ModelView v=new ModelView();
+        String jsp="redirection.jsp";
+        v.setUrl(jsp);
+        v.addAuthenf("isConnected", true);
+        v.addAuthenf("Profil", this.name);
+        return v;
+    }
 }
